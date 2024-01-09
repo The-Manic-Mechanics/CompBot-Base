706df40436b870ccd25b368c3db6be8bd7b31638
Leo Markley <stencylxd@theawesomeinter.net>
2024-01-09 15:43:04 -0500

    chore: update dependencies Also removed PathPlannerLib and the AutoAim code, following the API changes for 2024.
    If needed, we can remake using the system demonstrated here:
    https://docs.wpilib.org/en/stable/docs/software/pathplanning/trajectory-tutorial/creating-following-trajectory.html

--- src/main/java/frc/robot/commands/DriveAuton.java
+++ src/main/java/frc/robot/commands/DriveAuton.java
@@ -4,14 +4,14 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Gyroscope;
 
 /**
 * Used to Drive the robot during auton via the inputed speeds and distance
 */
-public final class DriveAuton extends CommandBase {
+public final class DriveAuton extends Command {
 	double 
 		driveInches, 
 		speedX, 
